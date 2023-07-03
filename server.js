204b1162db2540cc9ae3910d475a62fcf1388297
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2023-07-03 17:49:42 +0100

    update

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
-const httpProxy = require('http-proxy');
+// const httpProxy = require('http-proxy');
 
-const proxy = httpProxy.createProxyServer({
-  target: 'http://localhost:5000', // Replace with your JSON server URL
-  changeOrigin: true,
-});
+// const proxy = httpProxy.createProxyServer({
+//   target: 'http://localhost:5000', // Replace with your JSON server URL
+//   changeOrigin: true,
+// });
 
-module.exports = (req, res) => {
-  proxy.web(req, res);
-};
+// module.exports = (req, res) => {
+//   proxy.web(req, res);
+// };
 
 
 // JSON Server module
-// const jsonServer = require("json-server");
-// const server = jsonServer.create();
-// const router = jsonServer.router("db.json");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
 
-// // Make sure to use the default middleware
-// const middlewares = jsonServer.defaults();
+// Make sure to use the default middleware
+const middlewares = jsonServer.defaults();
 
-// server.use(middlewares);
-// // Add this before server.use(router)
-// server.use(
-//  // Add custom route here if needed
-//  jsonServer.rewriter({
-//   "/api/*": "/$1",
-//  })
-// );
-// server.use(router);
-// // Listen to port
-// server.listen(3000, () => {
-//  console.log("JSON Server is running");
-// });
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/api/*": "/$1",
+ })
+);
+server.use(router);
+// Listen to port
+server.listen(3000, () => {
+ console.log("JSON Server is running");
+});
 
-// // Export the Server API
-// module.exports = server;
+// Export the Server API
+module.exports = server;
 
